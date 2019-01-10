1567fbfcec2eac49bac78f8b8f0f2ffefaa42c53
Agathe-Balayn <Agathe.Balayn@ibm-com>
2019-01-10 18:51:41 +0100

    cleaned part of the code

--- aif360/metrics/inclusiveness_label_dataset_metric.py
+++ aif360/metrics/inclusiveness_label_dataset_metric.py
@@ -74,18 +74,65 @@ def annotation_plot_color(value):
 
 def plot_fairness(data, metric, bin_name=''):
 	yticks = list(data.index.values)
+
+	# Plot the bins
 	data = data.as_matrix(columns=[metric])
 	fig, ax = plt.subplots()
 	heatmap = ax.pcolor(data)
 	cbar = fig.colorbar(heatmap, ax=ax)
 	cbar.set_label(metric, rotation=90)
+	
 	ax.set_yticks(np.arange(data.shape[0]) + 0.5, minor=False)
 	ax.set_yticklabels(yticks,rotation=0)
 	ax.set_ylabel(bin_name)
 	ax.tick_params(left='off', bottom='off',labelbottom='off', color='grey',labelsize='small')
+
+	# Add the exact evaluation measure per bin
 	for i in range(data.shape[0]):
 	    text = ax.text(0.5, i+0.5, np.round(data[i][0], 2), ha="center", va="center", color=annotation_plot_color(data[i][0]))
+
 	fig.set_figwidth(4)	
+
+
+def define_fairness_bins(filtering_value, data_filtering_col, comparison_op, data, number_bins):
+	if filtering_value is not None:  
+		data = data[ops[comparison_op](data[data_filtering_col], filtering_value)]
+			
+	# Create the bins
+	if number_bins is None:
+		number_bins = 5 # default value
+	data['bin_col'], bin_col = pd.cut(data[data_filtering_col], number_bins, retbins=True)
+			
+	return data, bin_col
+
+
+def compute_bin_values(data, binning_entity, GT_col, prediction_col, eval_metrics):
+	bin_values = data.drop_duplicates(subset=binning_entity).reset_index()
+	bin_values = bin_values.join(bin_values.apply(lambda row: compute_evaluation_metric(data.loc[data[binning_entity] == row[binning_entity]][[GT_col, prediction_col]], GT_col, prediction_col, eval_metrics), axis = 1))
+	return bin_values
+
+def compute_bin_fairness(data, fairness_criteria, GT_col, prediction_col, eval_metrics, bin_col, binning_entity):
+	# Compute metric per bin
+	data = compute_bin_values(data, binning_entity, GT_col, prediction_col, eval_metrics)
+
+	# Aggregate to fairness bins if necessary.
+	if fairness_criteria == 'annotation_popularity':
+		data = data.set_index('bin_col')
+	elif fairness_criteria == 'demography':
+		data = data.set_index('pop_label')
+		list_pop_label = list(data.index.values)
+		data = data.groupby('pop_label').mean()
+	else:			
+		data = data.groupby('bin_col').mean()
+
+	# Reorder the bins
+	if fairness_criteria == 'demography':
+		data = data.loc[list_pop_label]
+	else:
+		data = data.loc[pd.IntervalIndex.from_breaks(np.round(bin_col,3).astype(np.float64)).values.tolist()]	
+
+	print("TODO: check whether bins are in order")
+	return data
 
 class InclusivenessLabelDatasetMetric(Metric):
 	"""Class for computing metrics based on a single
@@ -108,92 +155,54 @@ class InclusivenessLabelDatasetMetric(Metric):
 
 
 	
-
 	def compute_metric(self, prediction_col, GT_col, fairness_criteria, eval_metrics, number_bins=None, filtering_value=None, characterization=False):
-		#def fairness_metric_3(nb_bin,bin_criteria, bin_min, type_test_data, saving_folder, exp_name, remove_NaN, algo_name, dataset_distribution, training_agg, feature_type):
 
 		# Check whether the predictions of the model are contained in the dataset.  
 		if (prediction_col not in self.dataset.dataset.columns) or (GT_col not in self.dataset.dataset.columns):
 			raise KeyError("'dataset' should contain the predictions of the model and the ground truth data.")
 
+		# Type of fairness
 		if fairness_criteria == 'annotation_popularity':
 			data_filtering_col = 'popularity_percentage'
+			binning_entity = 'bin_col'
 			comparison_op = ">="
 		elif fairness_criteria == 'annotator_disagreement':
 			data_filtering_col = 'annotator_ADR'
 			comparison_op = "<="	        
+			binning_entity = 'worker_id'
 		elif fairness_criteria == 'sample_ambiguity':
 			data_filtering_col = 'sample_agreement' 
+			binning_entity = 'rev_id'
 			comparison_op = ">="	        
 		elif fairness_criteria == 'demography':
 			data_filtering_col = 'pop_label'
+			binning_entity = 'pop_label'
 			comparison_op = ">="
 
 		data = self.dataset.dataset
 
-		if fairness_criteria == 'annotation_popularity':
-			# Take out the bin ranges which are not studied  
-			if filtering_value is not None:  
-				data = data[data[data_filtering_col] >= filtering_value]
+		# Prepare the bins
+		if fairness_criteria != 'demography':
+			data, bin_col = define_fairness_bins(filtering_value, data_filtering_col, comparison_op, data, number_bins)
 			
-			# Create the bins
-			if number_bins is None:
-				number_bins = 5 # default value
-			data['bin_col'], bin_col = pd.cut(data[data_filtering_col], number_bins, retbins=True)
-			
-			### TODO put categories in order
-			print("TODO: put bins in order")
-			# Compute the metrics over the bins
-			unique_popularity = data.drop_duplicates(subset='bin_col').reset_index()
-			unique_popularity = unique_popularity[['bin_col']].join(unique_popularity[['bin_col']].apply(lambda row: compute_evaluation_metric(data.loc[data['bin_col'] == row['bin_col']][[GT_col, prediction_col]], GT_col, prediction_col, eval_metrics), axis = 1))
-			data = unique_popularity
-			data = data.set_index('bin_col')
-			data = data.loc[pd.IntervalIndex.from_breaks(np.round(bin_col,3).astype(np.float64)).values.tolist()]
-		elif fairness_criteria == 'demography':
+		else: # Demography: the bins correspond to the different types of annotators.
 			# Remove the lowest frequency demographics categories
 			if filtering_value is not None:  
 				data = data.groupby(data_filtering_col).filter(lambda x: len(x) >= filtering_value)
-			# Create the bins
-			unique_demography = data.drop_duplicates(subset='pop_label').reset_index()
-			unique_demography = unique_demography.join(unique_demography.apply(lambda row: compute_evaluation_metric(data.loc[data['pop_label'] == row['pop_label']][[GT_col, prediction_col]], GT_col, prediction_col, eval_metrics), axis = 1))
-			data = unique_demography
-			data = data.set_index('pop_label')
-			list_pop_label = list(data.index.values)
-			data = data.groupby(data_filtering_col).mean().loc[list_pop_label]
-		else:
-			if number_bins is None:
-				number_bins = 5 # default value
-			# Take out the bin ranges which are not studied    
-			if filtering_value is not None: 
-				data = data[ops[comparison_op](data[data_filtering_col], filtering_value)]
-			# Create the bins
-			data['bin_col'], bin_col = pd.cut(data[data_filtering_col], number_bins, retbins=True)
-
-			# Compute the performance per user or per sample
-			if fairness_criteria == 'sample_ambiguity':
-				unique_samples = data.drop_duplicates(subset='rev_id').reset_index()
-				unique_samples = unique_samples.join(unique_samples.apply(lambda row: compute_evaluation_metric(data.loc[data['rev_id'] == row['rev_id']][[GT_col, prediction_col]], GT_col, prediction_col, eval_metrics), axis = 1))
-				data = unique_samples
-			elif fairness_criteria == 'annotator_disagreement':
-				unique_annotators = data.drop_duplicates(subset='worker_id').reset_index()
-				unique_annotators = unique_annotators.join(unique_annotators.apply(lambda row: compute_evaluation_metric(data.loc[data['worker_id'] == row['worker_id']][[GT_col, prediction_col]], GT_col, prediction_col, eval_metrics), axis = 1))
-				data = unique_annotators
-			
-			data = data.groupby('bin_col').mean().loc[pd.IntervalIndex.from_breaks(np.round(bin_col,3).astype(np.float64)).values.tolist()]
+			bin_col = 'NA'
 		
+		# Compute the bin values	
+		data = compute_bin_fairness(data, fairness_criteria, GT_col, prediction_col, eval_metrics, bin_col, binning_entity)
+		
+		# Plot the bins
 		if characterization: # Output the characterizations plots per metric
 			for metric in eval_metrics: 
 				# Plot the distribution
 				plot_fairness(data, metric, bin_name=fairness_criteria)
 
-
+		# Compute the fairness values from the bins
 		dict_results = {}
 		for metric in eval_metrics: 
 			dict_results.update({metric:individual_fairness_values(data, metric)})	        
 	
 		return dict_results, data
-
-	
-
-	def compute_full_fairness_report():
-		return 
